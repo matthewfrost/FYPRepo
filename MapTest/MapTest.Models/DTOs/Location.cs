95db6e403567bc64b8000844e890a82ff2394b6b
Matthew Frost <l1426439@live.tees.ac.uk>
2016-10-28 21:51:13 +0100

    EF for maps

---
+++ MapTest/MapTest.Models/DTOs/Location.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MapTest.Models;
+
+namespace MapTest.Models.DTOs
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        private FYPracticeEntities db = new FYPracticeEntities();
+
+        public List<Location> getAll()
+        {
+            var Locations = db.Location.AsEnumerable();
+
+            List<Location> location = Locations.Select(l => new Location
+            {
+                ID = l.ID,
+                Name = l.Name,
+                Tag = l.TagName,
+                Latitude = l.Latitude,
+                Longitude = l.Longitude
+            }).ToList();
+
+            return location;
+        }
+
+        public void save()
+        {
+
+        }
+    }
+}

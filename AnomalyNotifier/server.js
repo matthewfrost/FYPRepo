bea899725350e4cf2846ccc4f77525c72a956641
Matthew Frost <l1426439@live.tees.ac.uk>
2017-04-02 21:12:23 +0100

    anomaly service returning values, added DB to git

--- AnomalyNotifier/server.js
+++ AnomalyNotifier/server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var tedious = require('tedious');
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
-var Parser = require('parser');
+var parser = require('body-parser');
 var app = express();
 var password = process.argv[2];
+var types = require('tedious').TYPES;
+var Location = require("./Location.js");
 
 var config = {
     userName: 'MatthewDev',
@@ -37,7 +39,7 @@ app.use(parser.urlencoded({
 app.use(parser.json());
 
 app.get('/getAnomalies', function (req, res) {
-    var lat, long, posLat, posLong, negLat, negLong, sql, request;
+    var lat, long, posLat, posLong, negLat, negLong, sql, request, data, currentLocation, currentIndex;
 
     var url = req.query;
 
@@ -46,22 +48,34 @@ app.get('/getAnomalies', function (req, res) {
 
     earthRadius = 6378;
 
-    posLat = latitude + (0.1 / earthRadius) * (180 / Math.PI);
-    posLong = longitude + (0.1 / earthRadius) * (180 / Math.PI) / Math.cos(latitude * Math.PI / 180);
-    negLat = latitude + (-0.100 / earthRadius) * (180 / Math.PI);
-    negLong = longitude + (-0.100 / earthRadius) * (180 / Math.PI) / Math.cos(latitude * Math.PI / 180);
+    posLat = lat + (0.1 / earthRadius) * (180 / Math.PI);
+    posLong = long + (0.1 / earthRadius) * (180 / Math.PI) / Math.cos(lat * Math.PI / 180);
+    negLat = lat + (-0.100 / earthRadius) * (180 / Math.PI);
+    negLong = long + (-0.100 / earthRadius) * (180 / Math.PI) / Math.cos(lat * Math.PI / 180);
 
     sql = 'dbo.getAnomalyLocations'
-
+    data = [];
+    currentLocation = '';
+    currentIndex = -1;
     request = new Request(sql, function (err, rowCount, rows) {
         if (err) {
             console.log(err);
         } else {
             var rowArray = [];
             rows.forEach(function (columns) {
-                var Location = new Location(columns[0].value, columns[1].value, columns[2].value, columns[3].value);
-                json.push(tag);
+                var location = new Location(columns[0].value, columns[1].value, columns[2].value, columns[3].value);
+                if(location.Name != currentLocation){
+                    data.push([]);
+                    currentLocation = location.Name;
+                    currentIndex++;
+                }
+                data[currentIndex].push(location);
             });
+
+            for(var i = 0; i < data.length; i++){
+                calculateStDev(data[i]);
+            }
+
             var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
             console.log("here " + ip);
             res.json(json);
@@ -77,3 +91,40 @@ app.get('/getAnomalies', function (req, res) {
 
     connection.callProcedure(request);
 });
+
+function calculateStDev(data) {
+    var total, mean, diffData, diff, sum;
+
+    total = 0;
+    diffData = [];
+    for (var i = 1; i < data.length; i++) {
+        diff = 0;
+        diff = data[i].Data - data[i - 1].Data;
+        diffData.push({ Location: data[i].Name, Value: diff, Timestamp: data[i].Timestamp });
+        total += diff;
+        
+    }
+    console.log(total);
+
+    mean = total / data.length;
+    var x, temp;
+    x = 0;
+    temp = 0;
+    for (var i = 0; i < diffData.length; i++) {
+        x = diffData[i].Value - mean;
+        temp += Math.pow(x, 2.0);
+    }
+    
+    var y = temp / diffData.length;
+    var stdDev = Math.pow(y, 0.5);
+    for (var i = 0; i < diffData.length; i++) {
+        var current = diffData[i];
+        if (current.Value > (mean + (2 * stdDev)) || current.Value < (mean - (2 * stdDev))) {
+            json.push(current);
+        }
+    }
+}
+
+app.listen(process.env.PORT || '8082');
+
+exports = module.exports = app;
